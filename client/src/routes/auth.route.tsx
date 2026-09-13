086f68891f3e24205a31d2c2474745ff889c9e09
0mairAftab <omairch035@gmail.com>
2026-09-13 10:30:18 +0500

    setup authorization for auth routes

--- client/src/routes/auth.route.tsx
+++ client/src/routes/auth.route.tsx
@@ -1,7 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { DashboardSkeleton } from "@/components/skeleton-loaders/dashboard-skeleton";
+import useAuth from "@/hooks/api/use-auth";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { isAuthRoute } from "./common/routePaths";
 
 const AuthRoute = () => {
-  return <Outlet />;
+  const location = useLocation();  //Get the current URL .. For example, if the user is currently visiting:  .login then location.pathname will be "/login"
+  const { data: authData, isLoading } = useAuth();
+  const user = authData?.user;
+
+  const _isAuthRoute = isAuthRoute(location.pathname);
+
+  if (isLoading && !_isAuthRoute) return <DashboardSkeleton />;  //If we're still checking whether the user is logged in, and they're trying to access a protected/dashboard route, show a loading skeleton.
+
+  if (!user) return <Outlet />;
+
+  return <Navigate to={`workspace/${user.currentWorkspace?._id}`} replace />;
 };
 
 export default AuthRoute;
